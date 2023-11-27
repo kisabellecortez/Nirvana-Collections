02690f463660b6451613d52e7db20df630e27015
kisabellecortez <kisabellecortez@gmail.com>
2023-11-26 22:35:10 -0500

    created bottom page banner

--- public-page/src/App.js
+++ public-page/src/App.js
@@ -3,7 +3,7 @@ import Sidebar from './components/Sidebar.js'
 
 import Logo from './assets/logo.png'
 import Name from './assets/logo-name.png'
-
+import Insta from './assets/instagram.svg'
 
 
 
@@ -16,6 +16,44 @@ function App() {
       <div className = "nav">
         <img className = "logo" src = {Logo} alt = "Nirvana Collections logo"></img>
         <img className = "name" src = {Name} alt = "Nirvana Collections"></img>
+      </div>
+
+      <div className = "cards">
+        
+      </div>
+
+      <div className = "cards">
+        
+      </div>
+
+      <div className = "cards">
+        
+      </div>
+
+      <div className = "end-banner">
+        <div className = "support">
+          <h1>SUPPORT</h1>
+            <a href = "/">Contact Us</a>
+            <a href = "/">FAQs</a>
+            <a href = "/">Jewelry Care</a>
+            <a href = "/">Ring Size Guide</a>
+        </div>
+        <div className = "more">
+          <h1>MORE</h1>
+            <a href = "/">About</a>
+            <a href = "/">Custom Orders</a>
+
+        </div>
+        <div className = "payments">
+
+        </div>
+        <div className = "socials">
+          <a href = "https://www.instagram.com/nirvanacollectinc/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">
+            <img src = {Insta} alt = "Instagram logo"></img>
+          </a>
+
+        </div>
+
       </div>
 
     </div>
