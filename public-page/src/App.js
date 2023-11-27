89b529589b4f013291a4112389e1b6d05afeee1f
kisabellecortez <kisabellecortez@gmail.com>
2023-11-26 20:12:31 -0500

    created animation for side menu

--- public-page/src/App.js
+++ public-page/src/App.js
@@ -3,19 +3,20 @@ import Sidebar from './components/Sidebar.js'
 
 import Logo from './assets/logo.png'
 import Name from './assets/logo-name.png'
-import Open from './assets/menu.svg'
-import Close from './assets/menu-x.svg'
+
+
+
 
 function App() {
+  
+
   return (
     <div className = "main">
+       <Sidebar/>
       <div className = "nav">
-        <img className = "menu-btn" src = {Open} alt = "open menu button"></img>
-        <img className = "menu-btn" src = {Close} alt = "close menu button"></img>
         <img className = "logo" src = {Logo} alt = "Nirvana Collections logo"></img>
         <img className = "name" src = {Name} alt = "Nirvana Collections"></img>
       </div>
-      <Sidebar/>
 
     </div>
     
