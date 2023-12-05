ef448ecd6c8b35751cc2c6b8477c1585da0eaff9
kisabellecortez <kisabellecortez@gmail.com>
2023-12-05 17:33:45 -0500

    developed styling for home page

--- website/public-page/src/App.js
+++ website/public-page/src/App.js
@@ -3,20 +3,12 @@ import Sidebar from './components/Sidebar.js'
 
 import Logo from './assets/logo.png'
 import Name from './assets/logo-name.png'
-import Insta from './assets/instagram.svg'
+import Insta from './assets/instagram.png'
+import Tiktok from './assets/tiktok.png'
 
-import { MongoClient } from 'mongodb';
-
+import carousel from './assets/carousel-filler.jpg'
 
 function App() {
-
-  const mongoClient = new MongoClient(
-    'mongodb+srv://kisabelle:20Kristina04@cluster0.x9kfe73.mongodb.net/nirvana-collections?retryWrites=true&w=majority'
-  )
-
-  const data = await mongoClient.db().collection('products').find({}).toArray(); 
-  
-  console.log('!!!', data);
 
   return (
     <div className = "main">
@@ -26,16 +18,22 @@ function App() {
         <img className = "name" src = {Name} alt = "Nirvana Collections"></img>
       </div>
 
-      <div className = "cards">
-        
-      </div>
 
-      <div className = "cards">
+      <div className = "home-page">
+        <div className = "new-arrivals">
         
-      </div>
+        </div>
 
-      <div className = "cards">
-        
+        <div className = "bestsellers">
+
+        </div>
+
+        <a className="about" href="/">
+          <h1>Welcome to Nirvana Collections</h1>
+          <p>Handmade crystal jewelry.</p>
+          <p>Intentionally made, ethically sourced.</p>
+        </a>
+
       </div>
 
       <div className = "end-banner">
@@ -49,6 +47,7 @@ function App() {
         <div className = "more">
           <h1>MORE</h1>
             <a href = "/">About</a>
+            <a href = "/">Blog</a>
             <a href = "/">Custom Orders</a>
 
         </div>
@@ -57,7 +56,10 @@ function App() {
         </div>
         <div className = "socials">
           <a href = "https://www.instagram.com/nirvanacollectinc/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">
-            <img src = {Insta} alt = "Instagram logo"></img>
+            <img className = "insta" src = {Insta} alt = "Instagram logo"></img>
+          </a>
+          <a href = "https://l.instagram.com/?u=https%3A%2F%2Fwww.tiktok.com%2F%40nirvanacollections%3F_t%3D8hv7gbADOT8%26_r%3D1&e=AT2TiNGlXAhKdyW5TV5zUk3pTd-c2Nk9lL5lf1bqopU5gaZJCpUOvTXflzOl8jjfhvznt-vYspB7vcpTpILLT4mV2vI8_gNR8xvu9kxoOC0Gbi-_WiZjzvW2fTmD-bOzldgkf_I">
+            <img className = "tiktok" src = {Tiktok} alt = "Tiktok logo"></img>
           </a>
 
         </div>
